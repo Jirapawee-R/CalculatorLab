876226fb16be9cb21692b08ee6f638d192cd4231
jirapawee_r <jirapawee_r@DESKTOP-035GL52>
2018-08-22 03:21:25 +0700

    Check point!

--- CPE200Lab1/CPE200Lab1/Form1.cs
+++ CPE200Lab1/CPE200Lab1/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Reflection;
 
 namespace CPE200Lab1
 {
@@ -15,6 +16,75 @@ namespace CPE200Lab1
         public Form1()
         {
             InitializeComponent();
+
+        }
+        double num, num2, result = 0;
+        int Operation = 0;
+
+        private void btnN_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (lblDisplay.Text == "0" || lblDisplay.Text == string.Format("{0:0}",num) || lblDisplay.Text == string.Format("{0:0}",result))
+            {
+                lblDisplay.Text = "";
+            }
+            if (lblDisplay.Text.Length <= 8)
+                lblDisplay.Text = lblDisplay.Text + btn.Text;
+            
+
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            num = double.Parse(lblDisplay.Text);
+            Operation = 1;
+            lblDisplay.Text = string.Format("{0:0}", num);
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            num = double.Parse(lblDisplay.Text);
+            Operation = 2;
+            lblDisplay.Text = string.Format("{0:0}", num);
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            num = double.Parse(lblDisplay.Text);
+            Operation = 3;
+            lblDisplay.Text = string.Format("{0:0}", num);
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            num = double.Parse(lblDisplay.Text);
+            Operation = 4;
+            lblDisplay.Text = string.Format("{0:0}", num);
+        }
+
+        private void btnPercent_Click(object sender, EventArgs e)
+        {
+            num2 = double.Parse(lblDisplay.Text);
+            num2 = num * num2 / 100;
+            lblDisplay.Text = string.Format("{0:0}", num2);
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            num2 = double.Parse(lblDisplay.Text);
+            result = 0;
+            lblDisplay.Text = string.Format("{0:0}", num2);
+            if (Operation == 1) result = num + num2;
+            else if (Operation == 2) result = num - num2;
+            else if (Operation == 3) result = num * num2;
+            else if (Operation == 4) result = num / num2;
+
+            lblDisplay.Text = string.Format("{0:0}", result);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = "0";
         }
     }
 }
